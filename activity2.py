56331c721476d2919069f2049e4a55dbd989566d
Arshad Ali <arshad@example.com>
2026-05-13 20:13:30 +0530

    hello

---
+++ activity2.py
@@ -0,0 +1,13 @@
+# Creating diffrent variables 
+x = 5
+y = "jhon"
+print(x)
+print(y)
+
+codingal ="codingal"
+print(codingal)
+
+"user input"
+name = input("enter your name: ")
+
+print("\nHello",name, "\nwelcome to codingal")
